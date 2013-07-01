87ea9754d4c7069142e64bd6c16cb7fb1b6de9c6
anton <akropp@gmail.com>
2013-07-01 12:54:28 -0400

    adding parser

---
+++ FParsecCombinators/Script.fsx
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#r "bin/Debug/FParsecCS.dll"
+#r "bin/Debug/FParsec.dll"
+
+
+#load "Program.fs"
+open FParsecCombinators
+
+// Define your library scripting code here
+
